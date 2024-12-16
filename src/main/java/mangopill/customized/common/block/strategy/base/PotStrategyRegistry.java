1196197cb03dc8e35e75aef5ec88132acbb4aea0
Mango_pill <132248671+Mangopill@users.noreply.github.com>
2024-12-16 15:27:51 +0800

    1.0a-2024-12-16-U-B

--- src/main/java/mangopill/customized/common/block/strategy/base/PotStrategyRegistry.java
+++ src/main/java/mangopill/customized/common/block/strategy/base/PotStrategyRegistry.java
@@ -1,6 +1,7 @@
 package mangopill.customized.common.block.strategy.base;
 
 import mangopill.customized.common.block.strategy.pot.*;
+import mangopill.customized.common.registry.ModBlockRegistry;
 import mangopill.customized.common.registry.ModItemRegistry;
 import net.minecraft.world.item.ItemStack;
 
@@ -8,7 +9,7 @@ public class PotStrategyRegistry {
     public static final PotStrategyHandler HANDLER = PotStrategyHandler.getInstance();
 
     public static void onPotRegistry() {
-        HANDLER.registry("block.customized.casserole",
+        HANDLER.registry(ModBlockRegistry.CASSEROLE.get().getDescriptionId(),
                 new LidStrategy(new ItemStack(ModItemRegistry.CASSEROLE_ILD.get()), true),
                 new DishStrategy(),
                 new DriveStrategy(),
