23ab71f25c308a627d8f626a7167cf470d1c6b45
Mangopill <1409933825@qq.com>
2025-05-05 10:49:17 +0800

    5/4

--- src/main/java/mangopill/customized/common/FoodValue.java
+++ src/main/java/mangopill/customized/common/FoodValue.java
@@ -13,8 +13,8 @@ public class FoodValue {
             .nutrition(4).saturationModifier(0.5F)
             .build();
 
+    public static final int SHORT = 2400;
     public static final int MEDIUM = 4800;
-    public static final int LONG = 9600;
 
     public static final FoodProperties TOMATO = new FoodProperties.Builder()
             .nutrition(2).saturationModifier(0.2F).build();
@@ -45,22 +45,22 @@ public class FoodValue {
     public static final FoodProperties TOMATO_AND_BEEF_BRISKET_SOUP = new FoodProperties.Builder()
             .nutrition(18).saturationModifier(1.0F).effect(
                     () -> new MobEffectInstance(MobEffects.SATURATION, MEDIUM, 1), 1.0F).effect(
-                    () -> new MobEffectInstance(ModEffectRegistry.ROBUST, LONG, 3), 1.0F).effect(
-                    () -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, LONG, 3), 1.0F).effect(
-                    () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, LONG, 3), 1.0F).build();
+                    () -> new MobEffectInstance(ModEffectRegistry.ROBUST, MEDIUM, 2), 1.0F).effect(
+                    () -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, SHORT, 2), 1.0F).effect(
+                    () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, MEDIUM, 2), 1.0F).build();
     public static final FoodProperties FISH_MAW_AND_CHICKEN_SOUP = new FoodProperties.Builder()
             .nutrition(16).saturationModifier(1.0F).effect(
                     () -> new MobEffectInstance(MobEffects.SATURATION, MEDIUM, 1), 1.0F).effect(
-                    () -> new MobEffectInstance(ModEffectRegistry.VITALITY_RESTORATION, LONG, 3), 1.0F).effect(
-                    () -> new MobEffectInstance(ModEffectRegistry.SUSTAINED_ENERGY, LONG, 3), 1.0F).build();
+                    () -> new MobEffectInstance(ModEffectRegistry.VITALITY_RESTORATION, SHORT, 2), 1.0F).effect(
+                    () -> new MobEffectInstance(ModEffectRegistry.SUSTAINED_ENERGY, MEDIUM, 2), 1.0F).build();
     public static final FoodProperties BRAISED_CHICKEN_WITH_SHIITAKE_MUSHROOMS_SOUP = new FoodProperties.Builder()
             .nutrition(16).saturationModifier(1.0F).effect(
                     () -> new MobEffectInstance(MobEffects.SATURATION, MEDIUM, 1), 1.0F).effect(
-                    () -> new MobEffectInstance(MobEffects.REGENERATION, LONG, 4), 1.0F).effect(
-                    () -> new MobEffectInstance(ModEffectRegistry.SMOOTH_BLOOD_FLOW, LONG, 4), 1.0F).build();
+                    () -> new MobEffectInstance(MobEffects.REGENERATION, SHORT, 2), 1.0F).effect(
+                    () -> new MobEffectInstance(ModEffectRegistry.SMOOTH_BLOOD_FLOW, MEDIUM, 2), 1.0F).build();
     public static final FoodProperties RADISH_AND_PORK_RIB_SOUP = new FoodProperties.Builder()
             .nutrition(17).saturationModifier(1.0F).effect(
                     () -> new MobEffectInstance(MobEffects.SATURATION, MEDIUM, 1), 1.0F).effect(
-                    () -> new MobEffectInstance(ModEffectRegistry.HYDRATION_AND_PLUMPNESS, LONG, 1), 1.0F).effect(
-                    () -> new MobEffectInstance(ModEffectRegistry.MENTAL_STIMULATION, LONG, 4), 1.0F).build();
+                    () -> new MobEffectInstance(ModEffectRegistry.HYDRATION_AND_PLUMPNESS, MEDIUM, 1), 1.0F).effect(
+                    () -> new MobEffectInstance(ModEffectRegistry.MENTAL_STIMULATION, MEDIUM, 2), 1.0F).build();
 }
