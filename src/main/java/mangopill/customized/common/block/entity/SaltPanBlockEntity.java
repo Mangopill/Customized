7180c6fa296692fed07dbb8aef5f88e9848ab33e
Mangopill <1409933825@qq.com>
2025-05-31 11:02:44 +0800

    5/31

--- src/main/java/mangopill/customized/common/block/entity/SaltPanBlockEntity.java
+++ src/main/java/mangopill/customized/common/block/entity/SaltPanBlockEntity.java
@@ -9,12 +9,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class SaltPanBlockEntity extends BlockEntity {
-    private final LazyOptional<SaltPanFluidHandler> fluidHandler;
+    private final LazyOptional<IFluidHandler> fluidHandler;
 
     public SaltPanBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntityTypeRegistry.SALT_PAN.get(), pos, state);
